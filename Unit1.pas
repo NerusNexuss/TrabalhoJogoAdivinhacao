a2a738f332b79c42d31d828a9d33f97cec2b95fb
NerusNexuss <108414450+NerusNexuss@users.noreply.github.com>
2025-04-16 21:51:33 -0300

    .
    .

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,17 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
 
 type
   TForm1 = class(TForm)
+    LbNomeJogo: TLabel;
+    EdtNumeroAdivinhar: TEdit;
+    BtnAdivinhar: TButton;
+    BtnNovoJogo: TButton;
+    LbAcertou: TLabel;
+    RadioGroup1: TRadioGroup;
+    procedure BtnAdivinharClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +27,28 @@ var
 implementation
 
 {$R *.dfm}
+function gerarNumero(gerarNumero: integer): integer;
+begin
+
+end;
+function sortearValorSecreto(nivel: Integer): Integer;
+begin
+  case nivel of
+    0:
+      sortearValorSecreto:= (Random(49)+1);
+    1:
+      sortearValorSecreto := (Random(99)+1);
+    2:
+      sortearValorSecreto := (Random(199)+1);
+
+  end;
+end;
+
+procedure TForm1.BtnAdivinharClick(Sender: TObject);
+  var
+    AdivinharNumero: integer;
+begin
+  AdivinharNumero := StrToInt(EdtNumeroAdivinhar.Text);
+end;
 
 end.
--- __recovery/Unit1.pas
+++ __recovery/Unit1.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.Imaging.jpeg;
 
 type
   TForm1 = class(TForm)
@@ -14,7 +15,11 @@ type
     BtnNovoJogo: TButton;
     LbAcertou: TLabel;
     RadioGroup1: TRadioGroup;
+    Image1: TImage;
     procedure BtnAdivinharClick(Sender: TObject);
+    procedure BtnNovoJogoClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Image1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -23,6 +28,7 @@ type
 
 var
   Form1: TForm1;
+  Sorteado: Integer;
 
 implementation
 
@@ -31,21 +37,46 @@ function gerarNumero(gerarNumero: integer): integer;
 begin
 
 end;
-function escolherDificuldade(escolhas: Double; escolha: Integer): Double;
+function sortearValorSecreto(nivel: Integer): Integer;
 begin
-  case escolha of
+  case nivel of
     0:
-      escolherDificuldade := randomize (Random(50));
+      sortearValorSecreto:= (Random(49)+1);
     1:
-      escolherDificuldade :=
+      sortearValorSecreto := (Random(99)+1);
     2:
-      escolherDificuldade :=
-  else
-    escolherDificuldade := escolhas;
+      sortearValorSecreto := (Random(199)+1);
+
   end;
 end;
 
 procedure TForm1.BtnAdivinharClick(Sender: TObject);
+  var
+    AdivinharNumero,Sortear,nivel: integer;
+    resultado:String;
+begin
+  AdivinharNumero := StrToInt(EdtNumeroAdivinhar.Text);
+
+
+  if ( AdivinharNumero = Sorteado)
+  resultado := ''
+
+end;
+
+procedure TForm1.BtnNovoJogoClick(Sender: TObject);
+  var
+    nivel: integer;
+begin
+   nivel = (RadioGroup1.ItemIndex);
+   Sorteado := sortearValorSecreto(nivel);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Image1Click(Sender: TObject);
 begin
 
 end;
